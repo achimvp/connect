582124181dd9199a933c333ca768e4b7d02746b9
achimvp <prittwitza@yahoo.com>
2019-08-08 19:12:44 +0200

    added favicon

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.prittwitz.xyz', '.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'www.prittwitz.xyz',
+    'www.achimvp.pythonanywhere.com',
+]
 
 
 # Application definition
